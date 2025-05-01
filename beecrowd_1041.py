d48849810fa10010a011a32f260d52696b6ee314
Vitor Igor <lessa.vitorigor@gmail.com>
2025-05-01 16:33:23 -0300

    new problems

--- beecrowd_1041.py
+++ beecrowd_1041.py
@@ -1,19 +1,24 @@
+def quadrante_ponto(posicao_x: float, posicao_y: float) -> str:
+    if posicao_x == 0 and posicao_y == 0:
+        return "Origem"
+    elif posicao_x > 0 and posicao_y > 0:
+        return "Q1"
+    elif posicao_x < 0 and posicao_y > 0:
+        return "Q2"
+    elif posicao_x < 0 and posicao_y < 0:
+        return "Q3"
+    elif posicao_x > 0 and posicao_y < 0:
+        return "Q4"
+    elif posicao_x == 0 and posicao_y != 0:
+        return "Eixo Y"
+    elif posicao_x != 0 and posicao_y == 0:
+        return "Eixo X"
+
 posicao_x, posicao_y = input().split()
 
 posicao_x = float(posicao_x)
 posicao_y = float(posicao_y)
 
-if posicao_x == 0 and posicao_y == 0:
-    print("Origem")
-elif posicao_x > 0 and posicao_y > 0:
-    print("Q1")
-elif posicao_x < 0 and posicao_y > 0:
-    print("Q2")
-elif posicao_x < 0 and posicao_y < 0:
-    print("Q3")
-elif posicao_x > 0 and posicao_y < 0:
-    print("Q4")
-elif posicao_x == 0 and posicao_y != 0:
-    print("Eixo Y")
-elif posicao_x != 0 and posicao_y == 0:
-    print("Eixo X")
+quadrante = quadrante_ponto(posicao_x, posicao_y)
+
+print(quadrante)
